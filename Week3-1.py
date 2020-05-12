cf03172f1888b5c16cbad3ba34520d81aa32e5e7
HaifaaNoubani <buthainanoubni@gmail.com>
2020-05-12 20:37:10 +0300

    thereed python part1

---
+++ Week3-1.py
@@ -0,0 +1,7 @@
+print(' Numbers from 1 to 10')
+myNumber=8.1
+inputNumper= float(input('"Guess the number:"'))
+while inputNumper!=myNumber:
+    inputNumper= float(input('"Guess the number:"'))
+    pass
+print('"Great! You did it!"')
